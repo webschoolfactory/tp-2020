f4e9a90d33071d4c584643ea6e7f3eb63fbd87d8
Loïc Calvy <lcalvy@gmail.com>
2017-01-16 15:08:41 +0100

    add codestyling

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
 
-server.listen(port, function () {
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, function() {
   console.log('Server listening at port %d', port);
 });
 
@@ -16,11 +19,11 @@ app.use(express.static(__dirname + '/public'));
 
 var numUsers = 0;
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+  socket.on('new message', function(data) {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
@@ -29,7 +32,7 @@ io.on('connection', function (socket) {
   });
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
+  socket.on('add user', function(username) {
     if (addedUser) return;
 
     // we store the username in the socket session for this client
@@ -47,21 +50,21 @@ io.on('connection', function (socket) {
   });
 
   // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
+  socket.on('typing', function() {
     socket.broadcast.emit('typing', {
       username: socket.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
+  socket.on('stop typing', function() {
     socket.broadcast.emit('stop typing', {
       username: socket.username
     });
   });
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function() {
     if (addedUser) {
       --numUsers;
 
